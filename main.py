83a38791f4cbccf18b4a53ebe07b2ddd8f72e373
Aditya <aditya@adi.laptop>
2021-03-09 17:22:29 +0530

    added a write to file feature

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from selenium import webdriver
 import bs4 as bs
 import time
+import os.path
+from os import path
+
 
 #Starting the browser
-browser = webdriver.Chrome('chromedriver') #change the path to the location of your driver
+browser = webdriver.Chrome('/home/aditya/Desktop/chromedriver') #change the path to the location of your driver
 
 #Getting the source code
 browser.get("https://www.youtube.com/playlist?list=PLLy_2iUCG87CNafffzNZPVa9rW-QmOmEv") #link to your playist
@@ -11,8 +14,15 @@ browser.get("https://www.youtube.com/playlist?list=PLLy_2iUCG87CNafffzNZPVa9rW-Q
 html_file = browser.page_source
 time.sleep(2)
 
-f = open("page_data.html","x") #this code will only create a file if a file with same name is NOT present in the directory
-f.write(html_file)
+
+if path.exists("page_data.html"):
+    f = open("page_data.html","w")
+    f.write(html_file)
+    
+else:
+    f = open("page_data.html","x") #this code will only create a file if a file with same name is NOT present in the directory
+    f.write(html_file)
+    
 f.close
 
 #closing the browser
@@ -32,11 +42,23 @@ data_list = []
 name_list = []
 url_list = []
 for i in range(len(anchor_tag)):
-    data.append(anchor_tag[i].attrs['aria-label'])
+    data_list.append(anchor_tag[i].attrs['aria-label'])
     url_list.append(anchor_tag[i].attrs['href'])
     name_list.append(anchor_tag[i].attrs['title'])
     
 
-print(data_list)
-print(name_list)
-print(url_list)
+#print(data_list)
+#print(name_list)
+#print(url_list)
+
+#Writing the data in a text file
+if path.exists("data.txt"):
+    f1 = open("data.txt",'w')
+    for i in range(len(data_list)):
+        f1.write("name :" + str(name_list[i]) + " Url: https://www.youtube.com/"+str(url_list[i]) + " \n")
+    f1.close
+else:
+    f1 = open("data.txt",'x')
+    for i in range(len(data_list)):
+        f1.write("name :" + str(name_list[i]) + " Url: https://www.youtube.com/"+str(url_list[i]) + " \n")
+    f1.close
